3fa63c544cc402ab1e9c70c5bcdf096ab4ed457d
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-07-08 19:37:19 +0530

    feat: initialize project and add user registration
    - Initial commit and project setup
    - Connected to MongoDB database
    - Created user model schema
    - Built create-account page and backend route

---
+++ src/app/api/create-account/route.ts
@@ -0,0 +1,52 @@
+import { connectDb } from "@/lib/db";
+import { User } from "@/models/userModel";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+
+connectDb();
+
+export const POST = async(request: NextRequest) => {
+  try {
+    const {email, username, password} = await request.json();
+    const normalizedEmail = email.toLowerCase();
+
+
+    // Check if user already exists
+    const existedUser = await User.findOne({email: normalizedEmail});
+
+    if(existedUser){
+      return NextResponse.json({error: "User already exists!"}, {status: 400});
+    }
+
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+
+    // create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    
+    return NextResponse.json(
+      {
+        message: "User created successfully",
+        success: true
+      }, 
+      {status: 201}
+    );
+
+
+  } catch (error: any) {
+    console.log("Error in user registration : ", error);
+    return NextResponse.json({error: error.message}, {status: 500});
+  }
+}
---
+++ src/app/create-account/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
+
+
+export default function createUserPage(){
+
+  const router = useRouter();
+
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+    password: ""
+  });
+
+  const [loading, setLoading] = useState(false);
+  
+
+  const onSignup = async() => {
+    try {
+      if(!user.email || !user.username || !user.password){
+        alert("All fields are required!");
+        return;
+
+      }
+
+      setLoading(true);
+      const response = await axios.post("/api/create-account", user);
+      console.log("Signup successful.", response.data);
+      router.push("/");   // login
+
+    } catch (error: any) {
+      console.log("Signup failed : ", error.message);
+
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
+  return(
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1 className="text-3xl font-semibold mb-5">
+          Create an account
+        </h1>
+        <label htmlFor="email">Email</label>
+        <input 
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          value={user.email}
+          onChange={(e) => setUser({...user, email: e.target.value})}
+          className="bg-gray-100 rounded-lg p-2 w-full"
+          />
+        <hr />
+        <label htmlFor="username">Username</label>
+        <input 
+          id="username"
+          type="text"
+          placeholder="Enter your username"
+          value={user.username}
+          onChange={(e) => setUser({...user, username: e.target.value})}
+          className="bg-gray-100 rounded-lg p-2 w-full"
+          />
+        <hr />
+        <label htmlFor="password">Password</label>
+        <input 
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+          value={user.password}
+          onChange={(e) => setUser({...user, password: e.target.value})}
+          className="bg-gray-100 rounded-lg p-2 w-full"
+          />
+        <hr />
+        <hr />
+        <button
+          onClick={onSignup}
+          className="bg-black text-white px-6 py-1.5 rounded-lg hover:bg-gray-600 cursor-pointer"
+        >
+          {loading ? "Processing" : "Signup"}
+        </button>
+        <hr />
+        <p>Already had a account? <Link href={"/"} className="underline text-blue-500">Login here</Link></p>
+      </div>
+    </div>
+  );
+}
